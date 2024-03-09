dec720963000c64159d224b4a281db4dba439049
avidcoder27 <gilireeves@gmail.com>
2024-03-09 12:36:14 -0500

    SHUFFLEbaord control for when jesse has no joystick

--- src/main/java/frc/team5115/Robot/Robot.java
+++ src/main/java/frc/team5115/Robot/Robot.java
@@ -1,23 +1,30 @@
 package frc.team5115.Robot;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PWM;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class Robot extends TimedRobot {
-    Joystick joy;
+    // Joystick joy;
+    GenericEntry fakeJoy;
     PWM actuator8;
     PWM actuator9;
     DigitalOutput out;
 
     @Override
     public void robotInit() {
-        joy = new Joystick(0);
+        // joy = new Joystick(0);
+        ShuffleboardTab shuffleboardTab = Shuffleboard.getTab("SmartDashboard");
+        fakeJoy = shuffleboardTab.add("Fake Joystick", 0.375).getEntry();
 
         actuator8 = new PWM(8);
         actuator8.setBoundsMicroseconds(2000, 1500, 1500, 1500, 1000);
@@ -25,14 +32,14 @@ public class Robot extends TimedRobot {
         actuator9 = new PWM(9);
         actuator9.setBoundsMicroseconds(2000, 1500, 1500, 1500, 1000);
 
-        new JoystickButton(joy, XboxController.Button.kB.value)
-        .onTrue(new InstantCommand(this :: deploy));
+        // new JoystickButton(joy, XboxController.Button.kB.value)
+        // .onTrue(new InstantCommand(this :: deploy));
 
-        new JoystickButton(joy, XboxController.Button.kY.value)
-        .onTrue(new InstantCommand(this :: epicButton));
+        // new JoystickButton(joy, XboxController.Button.kY.value)
+        // .onTrue(new InstantCommand(this :: epicButton));
 
-        new JoystickButton(joy, XboxController.Button.kA.value)
-        .onTrue(new InstantCommand(this :: stow));
+        // new JoystickButton(joy, XboxController.Button.kA.value)
+        // .onTrue(new InstantCommand(this :: stow));
     }
 
     private void stow() {
@@ -59,20 +66,24 @@ public class Robot extends TimedRobot {
     @Override
     public void teleopInit() {
         counter = 0;
-        
     }
 
     int counter;
 
     @Override
     public void teleopPeriodic() {
-        if (joy.getRawButton(XboxController.Button.kX.value)) {
-            double input = (joy.getRawAxis(XboxController.Axis.kLeftY.value) + 1) / 2.0;
-            actuator8.setPosition(input);
-            actuator9.setPosition(input);
-        }
-        counter ++;
+        // if (joy.getRawButton(XboxController.Button.kX.value)) {
+        //     double input = (joy.getRawAxis(XboxController.Axis.kLeftY.value) + 1) / 2.0;
+        //     actuator8.setPosition(input);
+        //     actuator9.setPosition(input);
+        // }
 
+        double input = fakeJoy.getDouble(0.550);
+        input = MathUtil.clamp(input, 0.40, 0.70);
+        actuator8.setPosition(input);
+        actuator9.setPosition(input);
+        
+        counter ++;
         if (counter % 20 == 0) {
             System.out.println("act8: " + actuator8.getPosition());
             System.out.println("act9: " + actuator9.getPosition());
